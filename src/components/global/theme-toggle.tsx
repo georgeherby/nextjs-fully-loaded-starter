99d903e56fc84f9ef9590e3fc5b41aa600fb6491
George Herbert <github@georgeherby.dev>
2024-07-08 17:40:23 +0100

    feat: fix theme button error

--- src/components/global/theme-toggle.tsx
+++ src/components/global/theme-toggle.tsx
@@ -1,16 +1,30 @@
 "use client";
 
-import { useTheme } from 'next-themes'
+import {useTheme} from 'next-themes'
 import {Button} from "@/components/ui/button";
 import {MoonIcon, SunIcon} from "@radix-ui/react-icons";
+import {useEffect, useState} from "react";
 
 const ThemeToggle = () => {
-  const { setTheme, theme } = useTheme()
+  const {setTheme, resolvedTheme} = useTheme()
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
     <div className={'flex space-x-10'}>
-      {theme ==='dark' ? <Button variant="link" size="icon" onClick={() => setTheme('light')}><SunIcon className="h-4 w-4" /></Button> : null}
-      {theme ==='light' ? <Button variant="link" size="icon" onClick={() => setTheme('dark')}><MoonIcon className="h-4 w-4" /></Button> : null}
+      {resolvedTheme === 'dark' ?
+        <Button variant="link" size="icon" onClick={() => setTheme('light')}><MoonIcon
+          className="h-4 w-4"/></Button>
+        :
+        resolvedTheme === 'light' ?
+          <Button variant="link" size="icon" onClick={() => setTheme('dark')}><SunIcon
+            className="h-4 w-4"/></Button>
+          : null}
     </div>
   )
 }
